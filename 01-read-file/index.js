b855a6548a91fc10ded91133dea60859c6199788
ssekridov@gmail.com <ssekridov@gmail.com>
2022-11-08 13:53:23 +0100

    fix: 01-read-file readStream implement

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,12 @@
 const fs = require('fs')
 const path = require('path')
+const { stdout } = process
 
-fs.readFile(
-  path.join(__dirname, 'text.txt'),
-  'utf-8',
-  (err, data) => {
-    if (err) throw err
-    console.log(data)
-  }
-)
+const fileDir = path.join(__dirname, 'text.txt')
+const output = fs.createReadStream(fileDir, 'utf-8')
+let data = ''
+
+output.on('data', chunk => {
+  data += chunk
+  console.log(data)
+})
